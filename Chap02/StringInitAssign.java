2ff8b283101bccbeba6d3979a576c47a6dd9f96e
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-05-01 18:35:59 +0900

    変数 => （文字列、数値)

---
+++ Chap02/StringInitAssign.java
@@ -0,0 +1,12 @@
+class StringInitAssign {
+
+	public static void main(String[] args) {
+		String s1 = "ABC";
+		String s2 = "XYZ";
+
+		s1 = "FBI";
+
+		System.out.println("String s1は" + s1 + "です。");
+		System.out.println("String s2は" + s2 + "です。");
+	}
+}
