7c56e38f6f0a4bd522ce2656382d6dedd9f7b1f8
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-05-07 00:14:18 +0900

    "と\'の利用した文字列リテラル

---
+++ Chap05/Quotation.java
@@ -0,0 +1,12 @@
+class Quotation {
+
+	public static void main(String[] args) {
+		System.out.println("文字列リテラルと文字定数について。");
+
+		System.out.println("20引数府で囲まれた\"ABC\"は文字列リテラルです。");
+
+		System.out.print("一重引数府で囲まれた");
+		System.out.print('\'');
+		System.out.println("A'は文字列リテラルです。");
+	}
+}
